cfe87315ee01bb83571a5c802a19086f84ec06ba
gvillar08 <78232517+gvillar08@users.noreply.github.com>
2021-03-31 04:50:43 -0700

    Delete PhaseEnd directory

--- PhaseEnd/src/main/java/com/example/PhaseEnd/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.PhaseEnd.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import com.example.PhaseEnd.entities.User;
-
-public interface UserRepository extends CrudRepository<User, Integer>
-{
-	User findByUser(String user);
-}
